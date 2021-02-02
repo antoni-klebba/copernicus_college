5c7bcfd6f6dfbe07413c04db580b4897fa6768fa
Antoni Klebba <antoni.klebba@gmail.com>
2021-02-02 12:12:01 +0100

    first commit. create simple structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
