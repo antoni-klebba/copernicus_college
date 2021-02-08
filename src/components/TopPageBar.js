28b033d6af605f585ffbba2b34de9b8e91bf97ec
Antoni Klebba <antoni.klebba@gmail.com>
2021-02-08 12:12:52 +0100

    add component

---
+++ src/components/TopPageBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../styles/components/TopPageBar.css";
+
+const TopPageBar = () => {
+  return <div className="top_page_bar"></div>;
+};
+
+export default TopPageBar;
